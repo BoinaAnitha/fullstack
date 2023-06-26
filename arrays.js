d84063b681611e50b9942fcb262add8bd8a0c40e
BoinaAnitha <137762755+BoinaAnitha@users.noreply.github.com>
2023-06-26 13:24:49 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var password = [12345, 4567, 6789];
+/*for (var i = 0; i < password.length; i++){
+     console.log(password[i]);
+}
+
+/*for (var X in password){
+    console.log(X);
+}*/
+for (var _i = 0, password_1 = password; _i < password_1.length; _i++) {
+    var x = password_1[_i];
+    console.log(x);
+}
